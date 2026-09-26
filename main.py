6e28f608c02ec2a96f7f3a3c5ee2a142a3465c27
Ivan <kur13ser72.bbbb@gmail.com>
2026-09-27 01:12:45 +0300

    Update main.py

--- main.py
+++ main.py
@@ -2,7 +2,6 @@ import platform, os, socket, sys, json, locale
 from datetime import datetime
 import psutil
 
-
 name_os = platform.system()
 arc = platform.machine()
 username = os.environ.get("USERNAME") or os.environ.get("USER")
@@ -20,36 +19,52 @@ memory_free = psutil.virtual_memory().free/(2**20)
 processes = []
 
 for process in psutil.process_iter(
-    attrs = ['pid', 'name', 'status', 'create_time', 'memory_info', 'cpu_percent'],
+    attrs = ['pid', 'name', 'status', 'create_time', 'memory_info'],
     ad_value=None
 ):  
-    if len(processes) == 10:
-        break
-    f = 0
-    memory_bytes = process.info['memory_info']
-    if memory_bytes is None:
-        f = 1
-    cpu = process.info['cpu_percent']
-    if cpu is None:
-        f = 1
-    if f == 0 and process.info['status'] == 'running':
-        process.info['memory_info'] = memory_bytes.rss/(2**20)
-        processes.append(process.info)
+    info_prog = process.info
+    pid = info_prog['pid']
+    name = info_prog['name']
+    status = info_prog['status']
+    create_time = info_prog['create_time']
+    try:
+        cpu_per = process.cpu_percent(interval=1)
 
+    except (
+        psutil.AccessDenied,
+        psutil.NoSuchProcess,
+        psutil.ZombieProcess
+    ):
+        continue
+    memory_info = info_prog['memory_info']
+    if (memory_info is not None) and (status == psutil.STATUS_RUNNING) and (cpu_per > 0):
+        memory_info = memory_info.rss/(2**20)
+        prog = {
+            'pid': pid,
+            'name': name,
+            'status': status,
+            'cpu_percent': cpu_per,
+            'memory_info': memory_info,
+            'create_time': create_time,
+        }
+        processes.append(prog)
+
+processes.sort(key=lambda proc: (proc["memory_info"], proc["cpu_percent"]), reverse=True)
+
+information = {
+    'OC': name_os,
+    'version_OS': info.release,
+    'Name_computer': info.node,
+    'Username': username,
+    'CPU_architecture': proc,
+    'count_cores': cores,
+    'RAM': memory,
+    'RAM_used': memory_used,
+    'RAM_free': memory_free,
+    'ip_pc': ip,
+    'Time': time.strftime("%d.%m.%Y %H:%M:%S"),
+    'Python_path': sys.executable,
+}
+
+print(information)
 print(processes)
-print(memory)
-print(memory_used)
-print(memory_free)
-print(info.node)
-print(info.release)
-print(info.version)
-print(username)
-print(ip)
-print(name_os)
-print(arc)
-print(proc)
-print(cores)
-print(sys.version)
-print(sys.executable)
-print(time)
-print(language)
